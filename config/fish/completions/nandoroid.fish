e138e9d442c1b1de648ebf1f6e0053e39b902cd2
rubberpirate <rahulridhu21@gmail.com>
2026-07-16 19:11:00 +0530

    added wm/shell/custom config -rp

---
+++ config/fish/completions/nandoroid.fish
@@ -0,0 +1,1 @@
+/home/rubberpirate/.local/share/nandoroid-cli/completions/nandoroid.fish
---
+++ config/fish/conf.d/inir-path.fish
@@ -0,0 +1,3 @@
+if not contains -- "/home/rubberpirate/.local/bin" $PATH
+    set -gx PATH "/home/rubberpirate/.local/bin" $PATH
+end
